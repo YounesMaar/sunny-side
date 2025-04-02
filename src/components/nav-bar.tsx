09be07ba2e4de2ebc0e71233d44705b865ca0064
unknown <younesmaaroufi67@gmail.com>
2025-04-02 17:03:14 +0000

    bulid

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,14 +1,10 @@
 import { navLinks } from "@/lib/utils";
 
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import {
   Sheet,
-  SheetClose,
   SheetContent,
   SheetDescription,
-  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
